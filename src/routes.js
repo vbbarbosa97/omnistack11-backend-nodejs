f9f2c0455493af9a72d12524488a55d3ec087d92
Vinicius Batista Barbosa <Vinicius.barbosa@medsenior.corp>
2020-04-12 11:53:52 -0300

    Adicionando validação com celebrate

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,63 @@
 const {Router} = require('express');
+const { celebrate, Segments, Joi} = require('celebrate');
 
+//Controller
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
+
 const routes = Router();
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.store);
+routes.post('/ongs', celebrate({
+    
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
 
-routes.post('/incidents', IncidentController.store);
+}), OngController.store);
+
+routes.post('/incidents', celebrate({
+
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required()
+    }),
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+
+}), IncidentController.store);
 routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete('/incidents/:id', celebrate({
 
-routes.get('/profile', ProfileController.index);
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
 
-routes.post('/session', SessionController.store);
+}), IncidentController.delete);
+
+routes.get('/profile', celebrate({
+
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+
+}), ProfileController.index);
+
+routes.post('/session', celebrate({
+
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required()
+    })
+
+}),SessionController.store);
 
 module.exports = routes;
